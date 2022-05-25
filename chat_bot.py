6f8a4dff68fed373181aba87188275b4dad98083
Pannagaperumal <pannagaperumal404@gmail.com>
2022-05-25 22:16:49 +0530

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -17,7 +17,7 @@ labels = [ ]
 responses = [ ] 
 
 
-for intent in data ['intents']:
+for intent in data ['intents']:                     #load sentences from the json file into train_sentences and train_labels
     for pattern in intent['patterns']:
         train_sentences.append(pattern)
         train_labels.append(intent['tag'])
@@ -41,16 +41,17 @@ oov_token = "<00V>"
 tokenizer = Tokenizer(num_words = vocab_size,oov_token = oov_token)
 tokenizer.fit_on_texts(train_sentences)
 word_index = tokenizer.word_index
-sequences =tokenizer.texts_to_sequences(train_sentences)
-padded_sequences = pad_sequences(sequences,truncating = 'post',maxlen = max_len)
+sequences =tokenizer.texts_to_sequences(train_sentences)        #converting text to sequence of charecters
+padded_sequences = pad_sequences(sequences,truncating = 'post',maxlen = max_len)    #removing the spaces between the characters
 
 ###model training##
+
 model =Sequential( )
 model.add(Embedding(vocab_size, embedding_dim,input_length = max_len))
 model.add(GlobalAveragePooling1D( ))
 model.add(Dense(16,activation ='relu'))
 model.add(Dense(16,activation ='relu'))
-model.add(Dense(num,activation = 'softmax'))
+model.add(Dense(num,activation = 'softmax'))    #classifying the input based on the labels 
 
 model.compile(loss = 'sparse_categorical_crossentropy',
              optimizer = 'adam',metrics = ['accuracy'])
@@ -62,13 +63,13 @@ epochs = 500
 history = model.fit(padded_sequences , np.array(train_labels),epochs = epochs)
 
 #save the trained model##
-model.save("chat_model")
+model.save("chat_model")        #saving the trained model
 
 import pickle
 
 #to save fitted tokenizer
 
-with open('F:/pycache/chatbot/tokenizer.pickle', 'wb') as handle:
+with open('F:/pycache/chatbot/tokenizer.pickle', 'wb') as handle:   #Saving the tokenized files
     pickle.dump(tokenizer, handle,protocol =pickle.HIGHEST_PROTOCOL )
     
 with open('F:/pycache/chatbot/label_encoder.pickle','wb') as ecn_file:
@@ -110,7 +111,7 @@ def chat():
                     break
                 result = model.predict(keras.preprocessing.sequence.pad_sequences
                                        (tokenizer.texts_to_sequences([inp]), truncating = 'post' , maxlen = max_len))
-                tag = lbl_encoder.inverse_transform([np.argmax(result)])
+                tag = lbl_encoder.inverse_transform([np.argmax(result)])    #converts the given text into label
                 
                 for i in data['intents']:
                     if i['tag'] == tag:
